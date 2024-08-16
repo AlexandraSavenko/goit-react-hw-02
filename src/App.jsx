05913980e90443aa2dca7b2704715375a5e2c95a
Alex <159346450+AlexandraSavenko@users.noreply.github.com>
2024-08-16 20:55:11 +0300

    updateFeedbacks to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
 import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
@@ -19,16 +19,32 @@ const App = () => {
         };
   });
 
+  const updateFeedbacks = (feedback) => {
+    setValue({ ...value, [feedback]: value[feedback] + 1 });
+  };
+  const resetClicks = () => {
+    setValue({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
   const totalFeedbacks = value.good + value.neutral + value.bad;
   const positiveFeedback =
     totalFeedbacks > 0 ? Math.round((value.good / totalFeedbacks) * 100) : 0;
+
   useEffect(() => {
     window.localStorage.setItem("feedbacks", JSON.stringify(value));
   }, [value]);
+
   return (
     <div>
       <Description />
-      <Options state={value} onChangeState={setValue} total={totalFeedbacks} />
+      <Options
+        onChangeState={updateFeedbacks}
+        total={totalFeedbacks}
+        onReset={resetClicks}
+      />
       {totalFeedbacks > 0 ? (
         <Feedback
           state={value}
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,28 +1,28 @@
 import css from "./Options.module.css";
-export default function Options({ state, onChangeState, total }) {
-  const updateFeedbacks = (feedback) => {
-    onChangeState({ ...state, [feedback]: state[feedback] + 1 });
-  };
-  const resetClicks = () => {
-    onChangeState({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
+export default function Options({ onChangeState, total, onReset }) {
+  // const updateFeedbacks = (feedback) => {
+  //   onChangeState({ ...state, [feedback]: state[feedback] + 1 });
+  // };
+  // const resetClicks = () => {
+  //   onChangeState({
+  //     good: 0,
+  //     neutral: 0,
+  //     bad: 0,
+  //   });
+  // };
   return (
     <div className={css.wrap}>
-      <button className={css.button} onClick={() => updateFeedbacks("good")}>
+      <button className={css.button} onClick={() => onChangeState("good")}>
         Good
       </button>
-      <button className={css.button} onClick={() => updateFeedbacks("neutral")}>
+      <button className={css.button} onClick={() => onChangeState("neutral")}>
         Neutral
       </button>
-      <button className={css.button} onClick={() => updateFeedbacks("bad")}>
+      <button className={css.button} onClick={() => onChangeState("bad")}>
         Bad
       </button>
       {total > 0 && (
-        <button className={css.button} onClick={resetClicks}>
+        <button className={css.button} onClick={onReset}>
           Reset
         </button>
       )}
